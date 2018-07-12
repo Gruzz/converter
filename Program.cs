ce9e8348b6f0060b5349f18f4483c7dd76864506
Gruzz <Neverfall86@gmail.com>
2018-07-13 01:53:00 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace converter
 {
@@ -10,68 +9,79 @@ namespace converter
     {
         static void Main(string[] args)
         {
-            bool a = true;
+            bool proverka = true;
             string decToBin = "";
-            while (a)
+            while (proverka)
             {
-                Console.WriteLine("enter decimal number greater than 0");
+                Console.WriteLine("enter a decimal integer between 0 and 2147483647");
                 decToBin = Console.ReadLine();
-                if (decToBin.All(char.IsDigit))
+                if (decToBin.All(char.IsDigit) && (int.TryParse(decToBin, out int b)))
                 {
-                    if (int.TryParse(decToBin, out int b))
-                    {
-                        if (int.Parse(decToBin) > 0)
-                        {
-                            a = false;
-                        }
-                        else
-                        {
-                            Console.WriteLine("number must be from 0 to 2147483647");
-                        }
-
-                    }
-                    else
-                    {
-                        Console.WriteLine("too big number. enter number from 0 to 2147483647");
-                    }
+                    proverka = false;
                 }
                 else
                 {
-                    Console.WriteLine("not a decimal number! Try again!");
+                    Console.WriteLine("Upps.. You're wrong. Try again. I know you can.");
+                }
+            }           
+            Console.WriteLine("binary number:" + ConvertDecToBin(decToBin));
+            proverka = true;
+            string binToDec = "";            
+            while (proverka)
+            {
+                Console.WriteLine("enter a binary number between 0 and 1111111111111111111111111111111");
+                binToDec = Console.ReadLine();
+                StringBuilder binStr = new StringBuilder("");
+                binStr.Append(binToDec);
+                int index = 0;
+                foreach (char c in binToDec)
+                {
+                    if (c < '0' || c > '1')
+                    {
+                        proverka = true;
+                        Console.WriteLine("Upps.. You're wrong. Try again. I know you can.");
+                        break;
+                    }
+                    if (binToDec[index] == '0')
+                        binStr.Remove(0, 1);
+                    proverka = false;
+                }
+                if (binStr.Length > 31)
+                {
+                    proverka = true;
+                    Console.WriteLine("Upps.. You're wrong. Try again. I know you can.");
                 }
             }
-            Console.WriteLine("binary number:" + ConvertDecToBin(decToBin));
-            Console.WriteLine("enter binary number");
-            string binToDec = Console.ReadLine();
             Console.WriteLine("decimal number:" + ConvertBinToDec(binToDec));
-            string ba= Console.ReadLine();
+            Console.WriteLine("Enter to exit");
+            string ba = Console.ReadLine();
         }
-        public static string ConvertDecToBin ( string decToBin )
+        public static StringBuilder ConvertDecToBin(string decToBin)
         {
             int ost = 0;
-            string binStr = "";
             int decInt = int.Parse(decToBin);
+            StringBuilder binNum = new StringBuilder("", 32);
             if (decInt == 1)
             {
-                binStr = "1";
+                binNum.Append( "1");
             }
             else if (decInt == 0)
             {
-                binStr = "0";
+                binNum.Append("0");
             }
             while (decInt > 1)
             {
                 ost = decInt % 2;
                 decInt = decInt / 2;
-                binStr = ost.ToString() + binStr;
+                binNum.Insert(0,ost.ToString());
                 if (decInt == 1)
                 {
-                    binStr = decInt.ToString() + binStr;
+                    binNum.Insert(0, decInt.ToString());
                 }
             }
-            return binStr;
+            return binNum;
         }
-        public static string ConvertBinToDec ( string binToDec)
+        public static string ConvertBinToDec(string binToDec)
         {
             int len = binToDec.Length;
             int num = len - 1;
